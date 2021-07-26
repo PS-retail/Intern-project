118d4691b4b16785790b13fdd9fad18325774d58
Daanial Ahmed <daaahmed@WKMZT3BF61E1.global.publicisgroupe.net>
2021-07-26 16:26:46 +0100

    generating twillo tokens

--- frontend/src/components/videoChat/videoChat.js
+++ frontend/src/components/videoChat/videoChat.js
@@ -1,9 +1,15 @@
 import React, { useState, useCallback } from 'react';
+import Lobby from './lobby';
+import Token, { videoToken } from './token'
+
+require('dotenv').config();
 
 const VideoChat = () => {
   const [username, setUsername] = useState('');
   const [roomName, setRoomName] = useState('');
   const [token, setToken] = useState(null);
+
+ 
 
   const handleUsernameChange = useCallback(event => {
     setUsername(event.target.value);
@@ -13,7 +19,39 @@ const VideoChat = () => {
     setRoomName(event.target.value);
   }, []);
 
-  return <div></div> // we'll build up our response later
+  
+
+  const handleSubmit = useCallback(async event => {
+    event.preventDefault();
+    const data = videoToken(username, roomName)
+    setToken(data.toJwt());
+  }, [username, roomName]);
+
+  const handleLogout = useCallback(event => {
+    setToken(null);
+  }, []);
+
+  let render;
+  if (token) {
+    render = (
+      <div>
+        <p>Username: {username}</p>
+        <p>Room name: {roomName}</p>
+        <p>Token: {token}</p>
+      </div>
+    );
+  } else {
+    render = (
+      <Lobby
+         username={username}
+         roomName={roomName}
+         handleUsernameChange={handleUsernameChange}
+         handleRoomNameChange={handleRoomNameChange}
+         handleSubmit={handleSubmit}
+      />
+    );
+  }
+  return render;
 };
 
 export default VideoChat;
