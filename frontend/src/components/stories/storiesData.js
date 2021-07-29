fed409c287ccfa0b279ddd5b633ec9a0c2d72435
alexwavrant <wavrant.alex@gmail.com>
2021-07-29 14:55:55 +0100

    storiesData added

---
+++ frontend/src/components/stories/storiesData.js
@@ -0,0 +1,95 @@
+
+    const storiesData = [
+        {
+          id: 1,
+          name: "Home Cinema Room",
+          type: "AR-Story",
+          productsIncluded: 
+                          [       
+                                {product: "Beo-Vision Eclipse", link: "",}
+                          ],
+          tagline: "Hear. See. Feel.",
+          description: "The ultimate cinematic experience in your home. Hearing is believing. Because, when it’s played just right, sound has the incredible ability to transport you from your living room to a packed stadium, to the bottom of the ocean – or a galaxy far far away.",
+          link: "https://ps-retail-cinema.glitch.me",
+        },  
+        
+        {
+          id: 2,
+          name: "House Party",
+          type: "AR-Story",
+          productsIncluded: 
+                          [       
+                                {product: "Beo-Lab 5", link: "",},
+                                {product: "Beo-Sound 2", link: "",},
+                                {product: "Beo-Sound Edge", link: "",},
+                          ],
+          tagline: "Amplify the Celebration. Clarify your Relaxation",
+          description: "Working from home. Partying from Home. We have you covered. Turn a loud night in can become zen in a matter of seconds. ",
+          link: "https://ps-retail-cinema.glitch.me",
+        },
+        
+        {
+          id: 3,
+          name: "Day to Night Childcare",
+          type: "AR-Story",
+          productsIncluded: 
+                          [       
+                                {product: "Beo-Sound 2", link: "",}
+                          ],
+          tagline: "",
+          description: "",
+          link: "https://day2night.glitch.me",  
+        },
+        
+        {
+          id: 4,
+          name: "Beo-Sound Edge",
+          type: "AR-Product",
+          productsIncluded: 
+                          [       
+                                {product: "Beo-Sound Edge", link: "",}
+                          ],
+          tagline: "Sound revolution",
+          description: "BeoSound Edge is a compact and powerful wireless speaker, which through magical interaction, timeless design and cutting-edge acoustic innovation, offers a mesmerising listening experience.",
+          link: "https://beo-sound-edge-floorstand.glitch.me",            
+        },
+        {
+          id: 5,
+          name: "Beo-Sound 2",
+          type: "AR-Product",
+          productsIncluded: 
+                          [       
+                                {product: "Beo-Sound 2", link: "",}
+                          ],
+          tagline: "Home is where the music is",
+          description: "Powerful multiroom speaker sounding every bit as beautiful as it looks. Captivating no matter where you place it or how you use it.",
+          link: "https://beo-sound2.glitch.me",            
+        },
+        {
+          id: 6,
+          name: "Beo-Lab 50",
+          type: "AR-Product",
+          productsIncluded: 
+                          [       
+                                {product: "Beo-Lab 5", link: "",}
+                          ],
+          tagline: "The future of sound",
+          description: "High-end active loudspeaker blending radical audio technology with graceful aesthetics and meticulous craftsmanship.",
+          link: "https://beolab50.glitch.me",            
+        },
+    
+        {
+          id: 7,
+          name: "Beo-Vision Eclipse",
+          type: "AR-Product",
+          productsIncluded: 
+                          [       
+                                {product: "Beo-Vision Eclipse", link: "",}
+                          ],
+          tagline: "Crafted sound and design harmony",
+          description: "With the world’s finest TV sound system, Beovision Eclipse TV is designed to integrate beautifully with your lifestyle.",
+          link: "https://beo-vision-eclipse-floorstand.glitch.me",            
+        },
+    ]
+    
+    export default storiesData
