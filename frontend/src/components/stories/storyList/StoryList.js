81a096a3c11a12aef12bb2d9bc78e9f8e02f5000
isha <isha.upadhyay2016@gmail.com>
2021-07-30 14:42:44 +0100

    size

--- frontend/src/components/stories/storyList/StoryList.js
+++ frontend/src/components/stories/storyList/StoryList.js
@@ -9,7 +9,7 @@ function StoryList() {
         <div style={{ 
              backgroundImage: `url("https://images.unsplash.com/photo-1504280539878-538b933c05b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80")`, 
              resizeMode:'center',
-             height: 1500,
+             height: 1000,
              width: 1791
     
             }}>
