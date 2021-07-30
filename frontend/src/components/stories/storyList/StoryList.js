6ebb6fb738c6822ba8938542befff9fe4438aad0
isha <isha.upadhyay2016@gmail.com>
2021-07-30 18:39:43 +0100

    Changed the background

--- frontend/src/components/stories/storyList/StoryList.js
+++ frontend/src/components/stories/storyList/StoryList.js
@@ -1,20 +1,35 @@
 import React from "react";
 // import styled from "styled-components";
 // import tw from "twin.macro";
-
+import "./storyList.css";
 
 function StoryList() {
     return (
         <main>
-        <div style={{ 
-             backgroundImage: `url("https://images.unsplash.com/photo-1504280539878-538b933c05b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80")`, 
-             resizeMode:'center',
-             height: 1000,
-             width: 1791
-    
-            }}>
-            
-        </div>
+            <div className = "Wrapper">
+                <img
+                 className="background"
+                 src="https://images.unsplash.com/photo-1520170350707-b2da59970118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1301&q=80"
+                 alt="logo"             
+                >
+                </img>
+             </div>
+             <div className="center">
+                 COME EXPERIENCE OUR PRODUCTS AR!
+             </div>
+        <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+            {/* <div className="party-container">
+            <img
+                 className="background"
+                 src="https://images.unsplash.com/photo-1504280539878-538b933c05b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1791&q=80"
+                 alt="logo"             
+                >
+                </img>
+
+            </div> */}
         </main>
     )
 }
