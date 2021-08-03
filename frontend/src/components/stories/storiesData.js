b54ee9fc40cccbad5e2bb4fff119d9a4910f61f2
alexwavrant <wavrant.alex@gmail.com>
2021-08-03 09:25:17 +0100

    Removed products from storiesData.js

--- frontend/src/components/stories/storiesData.js
+++ frontend/src/components/stories/storiesData.js
@@ -33,56 +33,6 @@
           link: "https://day2night.glitch.me",  
           bgImage: "https://source.unsplash.com/eJwSOguD1rE/1920x1280"
         },
-        
-        {
-          id: 4,
-          name: "Beo-Sound Edge",
-          type: "AR-Product",
-          productsIncluded: 
-                          [       
-                                {product: "Beo-Sound Edge", link: "",}
-                          ],
-          tagline: "Sound revolution",
-          description: "BeoSound Edge is a compact and powerful wireless speaker, which through magical interaction, timeless design and cutting-edge acoustic innovation, offers a mesmerising listening experience.",
-          link: "https://beo-sound-edge-floorstand.glitch.me",            
-        },
-        {
-          id: 5,
-          name: "Beo-Sound 2",
-          type: "AR-Product",
-          productsIncluded: 
-                          [       
-                                {product: "Beo-Sound 2", link: "",}
-                          ],
-          tagline: "Home is where the music is",
-          description: "Powerful multiroom speaker sounding every bit as beautiful as it looks. Captivating no matter where you place it or how you use it.",
-          link: "https://beo-sound2.glitch.me",            
-        },
-        {
-          id: 6,
-          name: "Beo-Lab 50",
-          type: "AR-Product",
-          productsIncluded: 
-                          [       
-                                {product: "Beo-Lab 5", link: "",}
-                          ],
-          tagline: "The future of sound",
-          description: "High-end active loudspeaker blending radical audio technology with graceful aesthetics and meticulous craftsmanship.",
-          link: "https://beolab50.glitch.me",            
-        },
-    
-        {
-          id: 7,
-          name: "Beo-Vision Eclipse",
-          type: "AR-Product",
-          productsIncluded: 
-                          [       
-                                {product: "Beo-Vision Eclipse", link: "",}
-                          ],
-          tagline: "Crafted sound and design harmony",
-          description: "With the world’s finest TV sound system, Beovision Eclipse TV is designed to integrate beautifully with your lifestyle.",
-          link: "https://beo-vision-eclipse-floorstand.glitch.me",            
-        },
     ]
     
     export default storiesData
