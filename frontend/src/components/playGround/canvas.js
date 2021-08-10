8f35a609275cf2e01e19032138b3f92d9eeec06b
Daanial Ahmed <daaahmed@WKMZT3BF61E1.global.publicisgroupe.net>
2021-08-10 17:25:48 +0100

    can draw on model

--- frontend/src/components/playGround/canvas.js
+++ frontend/src/components/playGround/canvas.js
@@ -2,72 +2,62 @@ import React, { useRef, useEffect } from 'react'
 import '@google/model-viewer';
 import tw from "twin.macro";
 import styled from "styled-components";
+import { red } from '@material-ui/core/colors';
+import ReactDOM from "react-dom";
+import CanvasDraw from "react-canvas-draw";
 
 
 const CanvasContainer = styled.div`
-  ${tw`
-    z-0
-    absolute left-0 top-0
-  `};
-`;
-
-const ModelContainer = styled.div`
   ${tw`
     z-10
     absolute left-0 top-0
   `};
 `;
 
-const TabContainer = styled.div`
+const ModelContainer = styled.div`
   ${tw`
-    relative
+    z-0
+    absolute left-0 top-0
+    h-full
+    w-full
   `};
 `;
 
-const Canvas = props => {
+const TabContainer = styled.div`
+  ${tw`
+    relative
+    
+  `};
+`;
+
+const ModelViewerStyle = {
+  width: "400px",
+  height: "600px"
+}
+
+
+
+const Canvas = () => {
   
-  const canvasRef = useRef(null)
-
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#000000'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill()
-  }
+  const canvasRef = React.useRef(null)
 
   
-  useEffect(() => {
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
-    //Our first draw
-    
-
-    let frameCount = 0
-    let animationFrameId
-    
-    //Our draw came here
-    const render = () => {
-      frameCount++
-      draw(context, frameCount)
-      animationFrameId = window.requestAnimationFrame(render)
-    }
-    render()
-    
-    return () => {
-      window.cancelAnimationFrame(animationFrameId)
-    }
-
-  }, [draw])
   
   return(
     <TabContainer>
-      <CanvasContainer>
-        <canvas ref={canvasRef} {...props}/>
-      </CanvasContainer>
-      <ModelContainer>
-        <model-viewer src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.glb?1542147958948" alt="A 3D model of an astronaut" ar-modes="webxr scene-viewer quick-look" environment-image="neutral" auto-rotate camera-controls></model-viewer>
-      </ModelContainer>
+
+      
+      <model-viewer 
+        src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.glb?1542147958948" 
+        alt="A 3D model of an astronaut" 
+        ar-modes="webxr scene-viewer quick-look" 
+        environment-image="neutral" 
+        auto-rotate
+        style={ModelViewerStyle}
+        >
+
+        <CanvasDraw hideGrid='true' backgroundColor = '' brushRadius= '3' lazyRadius = '0' canvasWidth = {ModelViewerStyle.width} canvasHeight = {ModelViewerStyle.height}></CanvasDraw>
+      </model-viewer>
     </TabContainer> 
   )
 
--- frontend/src/components/playGround/playGroundPage.js
+++ frontend/src/components/playGround/playGroundPage.js
@@ -1,91 +1,75 @@
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import { io } from "socket.io-client";
-import '@google/model-viewer';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Tab from "@material-ui/core/Tab";
+import TabContext from "@material-ui/lab/TabContext";
+import TabList from "@material-ui/lab/TabList";
+import TabPanel from "@material-ui/lab/TabPanel";
+import Box from "@material-ui/core/Box";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
+import AccessibilityIcon from "@material-ui/icons/Accessibility";
+import { makeStyles } from "@material-ui/core";
 import Canvas from './canvas';
+import '@google/model-viewer';
 
-
-const socket = io("http://localhost:5000");
-
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
+const useStyles = makeStyles({
+  appContainer: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100vw",
+    height: "100vh"
   },
-}));
 
-function PlayGroundPage() {
+  container: {
+    display: "flex",
+    height: "100%",
+    width: "100%"
+  },
+  panel: {
+    width: "100%"
+  }
+});
+export default function PlayGroundPage() {
+  const [value, setValue] = React.useState("1");
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Items" {...a11yProps(0)} />
-          <Tab label="Drawing Canvas" {...a11yProps(1)} />
+    <Box className={classes.appContainer}>
+      <TabContext value={value}>
+        <AppBar position="static">
+          <TabList
+            variant="scrollable"
+            onChange={handleChange}
+            aria-label="tabs"
+          >
+            <Tab label="Models" value="1"/>
+            <Tab label="Canvas" value="2" />
 
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        
-        <Canvas />
-        
-      </TabPanel>
-    
-    </div>
+            
+          </TabList>
+        </AppBar>
+
+        <Box className={classes.container}>
+          <TabPanel value="1" className={classes.panel}>
+            <Box
+              className={classes.container}
+              style={{ backgroundColor: "red" }}
+            >
+              Content 1
+            </Box>
+          </TabPanel>
+          <TabPanel value="2" className={classes.panel}>
+            <Box
+              className={classes.container}
+            >
+              <Canvas></Canvas>
+            </Box>
+          </TabPanel>
+          
+        </Box>
+      </TabContext>
+    </Box>
   );
 }
-
-
-export default PlayGroundPage;
